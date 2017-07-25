65a2d8c743bc5aa7de65a82acf70c725a92cc717
Bharat.Mehta <Bharat.Mehta@HC13-0271.hcentive.com>
2017-07-25 14:00:50 +0530

    More code changes

--- src/main/java/com/bharatmehta/webcrawler/WebcrawlerApplication.java
+++ src/main/java/com/bharatmehta/webcrawler/WebcrawlerApplication.java
@@ -2,10 +2,35 @@ package com.bharatmehta.webcrawler;
 
 import java.net.MalformedURLException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class WebcrawlerApplication {
 
-	public static void main(String[] args) throws NumberFormatException, MalformedURLException {
+	private static final Logger LOGGER = LogManager.getLogger(WebcrawlerApplication.class.getName());
+	
+	public static void main(String[] args) throws  MalformedURLException {
+		Integer maxDepth = 100;
+		Integer maxLinks = 50000;;
 		
-		new PageCrawler(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+		try{
+			 maxDepth = Integer.parseInt(args[1]);
+		}catch(NumberFormatException e){
+			LOGGER.error(e);
+		}catch(IndexOutOfBoundsException e) {
+			LOGGER.error(e);
+		}
+		
+		try{
+			 maxLinks = Integer.parseInt(args[2]);
+		}catch(NumberFormatException e){
+			LOGGER.error(e);
+		}catch(IndexOutOfBoundsException e) {
+			LOGGER.error(e);
+		}
+		
+		String urlLink   = args[0];
+		PageCrawler crawler  = new PageCrawler(urlLink, maxDepth, maxLinks);
+		crawler.crawl();
 	}
 }
